ab9b7c47576ce7d21909b7b34238b3471f359263
mu <semkadarov@gmail.com>
2024-04-04 21:41:37 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Task1");
+        int clientOs = 1;
+        switch (clientOs) {
+            case 0:
+                System.out.println("Установите версию приложения для iOS по ссылке");
+              break;
+            case 1:
+                System.out.println("Установите версию приложения для Android по ссылке");
+                break;
+        }
+
     }
 }
