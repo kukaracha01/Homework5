302235dfbdb5db9eb515fa9ad388b8f6d40fa1a4
mu <semkadarov@gmail.com>
2024-04-06 20:51:34 +0300

    Задание 3 (из конченных)

--- src/Main.java
+++ src/Main.java
@@ -32,5 +32,16 @@ public class Main {
                     break;
             }
         }
+
+        System.out.println("Task3");
+        int year = 2000;
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0 ) {
+                System.out.println(year + " год является високосным");
+            }
+        else {
+            System.out.println(year + " год является не високосным");
+        }
+
+
     }
 }
